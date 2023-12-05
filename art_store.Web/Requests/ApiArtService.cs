060ad07e65cb697a2a2b62a546d5eabc69ec1748
nikivikk <nikiforenko2110@gmail.com>
2023-12-05 11:19:39 +0300

    added simple web project

---
+++ art_store.Web/Requests/ApiArtService.cs
@@ -0,0 +1,44 @@
+﻿using art_store.art_storeDto;
+using art_store.Services.Contract;
+using System.Net.Http.Json;
+
+
+namespace art_store.Web.Requests
+{
+    public class ApiArtService : IArtService
+    {
+        protected readonly HttpClient _httpClient;
+
+        public ApiArtService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<int> Create(ArtDto art)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ArtDto> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ArtDto>> GetAll()
+        {
+
+            var response = await _httpClient.GetFromJsonAsync<List<ArtDto>>($"Art");
+            return response ?? throw new HttpRequestException("Couldn't get arts");
+        }
+
+        public Task<int> Update(ArtDto art)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- art_store.art_storeDto/CreateUserDto.cs
+++ art_store.art_storeDto/CreateUserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace art_store.art_storeDto
 {
     public class CreateUserDto
     {
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
         public string Fio { get; set; }
-        public string Role { get; set; }
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
         public string Password { get; set; }
+        public string Role { get; set; }
+        [Required]
+        [StringLength(3, MinimumLength = 1)]
+
         public int Age { get; set; }
     }
 }
